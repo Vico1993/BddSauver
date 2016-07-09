80d3f2e265ebb980b6b205c547d8009a38987a07
Victor Piolin <victor.piolin@gmail.com>
2016-07-09 23:21:53 +0200

    test

--- saver.sh
+++ saver.sh
@@ -5,11 +5,13 @@ IP="163.172.39.22"
 PORT='3306'
 PASSWORD="victor1993"
 
-databases=`mysql -h $IP -P $PORT -u $USER --password=$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+#databases=`mysql -h $IP -P $PORT -u $USER --password=$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
-for db in $databases; do
-    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
-        echo "Dumping database: $db"
-        mysqldump -u $USER -p$PASSWORD --databases $db > ./save/`date +%Y%m%d`.$db.sql
-    fi
-done
+mysql -h $IP -P $PORT -u $USER --password=$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database
+
+# for db in $databases; do
+#     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
+#         echo "Dumping database: $db"
+#         mysqldump -u $USER -p$PASSWORD --databases $db > ./save/`date +%Y%m%d`.$db.sql
+#     fi
+# done
